85f8ce01cbb68112ec1c4ffc7384761095e57a72
T. Leonard <129186412+Tam-Leonard@users.noreply.github.com>
2023-05-10 15:28:14 -0500

    Add files via upload

---
+++ crew/crew.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crew',
+  templateUrl: './crew.component.html',
+  styleUrls: ['./crew.component.css']
+})
+export class CrewComponent implements OnInit {
+ 
+  memberBeingEdited: object = null;
+  crew: object[] = [
+    {name: "Eileen Collins", firstMission: false},
+    {name: "Mae Jemison", firstMission: false},
+    {name: "Ellen Ochoa", firstMission: true}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    for(let member of this.crew) {
+      if (member['name'] === memberName){
+        return;
+      }
+    }
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+
+}
